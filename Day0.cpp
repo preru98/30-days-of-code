fe8b1a06b46167719f9245ec18b61d2bc792488f
Prerna <prerna1998.ps@gmail.com>
2020-03-15 23:10:57 +0530

    Day 0

---
+++ Day0.cpp
@@ -0,0 +1,13 @@
+include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    string a;
+    getline(cin,a);
+    cout<<"Hello, World.\n";
+    cout<<a;
+    return 0;
+}

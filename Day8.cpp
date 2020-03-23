6b85a76d609a5f80572841a41ff7167ca7a3947d
Prerna <prerna1998.ps@gmail.com>
2020-03-23 23:58:19 +0530

    Day 08.Part II

--- Day8.cpp
+++ Day8.cpp
@@ -1,38 +1,39 @@
 /*
 
-This solution is not employing any complex data structure, but follows standard method of storage 
-and retrieval.Therefore it produces correct output but takes longer than required.This question must 
-be solved using some data structure. Both the solution are given.One with maps( next commit ) and this one.
+The solution in the previous commit was simpler one but taking more than given time. This solution uses map.
+Both the solution are given.One with maps( this one ) and simpler one in previous commit.
 
 */
 
-
+#include <cmath>
+#include <cstdio>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include<map>
+#include<iterator>
+#include<string>
 using namespace std;
 
 
 int main() {
     int d_size;
     cin>>d_size;
-    vector<string> names(d_size), numbers(d_size);
-    string s;
-
+    map<string,string> ph_book;
+    string name, number,s;
+    // ph_book.insert({"ref", "fa"}); 
     for(int i=0;i<d_size;i++){
-        cin>>names[i];
-        cin>>numbers[i];
+        cin>>name;
+        cin>>number;
+        ph_book.insert({name, number});
     }
 
+
     while(cin>>s){
-        int flag=1;
-        for(int i=0;i<d_size;i++){
-            if(names[i]==s){
-                flag=0;
-                cout<<names[i]<<"="<<numbers[i]<<"\n";
-                break;
-            }
+        if(ph_book.find(s)!=ph_book.end()){
+            cout<<ph_book.find(s)->first<<"="<<ph_book.find(s)->second<<"\n";
         }
-        if(flag){
+        else{
             cout<<"Not found\n";
         }
     } 

08f595a913486c6959a064342de5cd64c811635c
Prerna <prerna1998.ps@gmail.com>
2020-03-31 17:34:02 +0530

    Day 16

---
+++ Day16.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string S;
+    cin >> S;
+    try{
+        cout<<stoi(S);
+    }
+    catch(...){
+        cout<<"Bad String";
+    }
+    return 0;
+}
